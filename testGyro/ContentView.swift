3ba674e728775b698dd57ffbe7176b93258298e8
ohsuyuki <papu8osu@gmail.com>
2020-10-18 17:07:52 +0900

    Initial Commit

---
+++ testGyro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testGyro
+//
+//  Created by yuuki oosu on 2020/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
